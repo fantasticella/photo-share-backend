23fa76ca62bd9307a70207ce3b0737302a61a6d1
fantasticella <105017921+fantasticella@users.noreply.github.com>
2025-07-06 16:40:07 -0700

    Ignore env and node_modules

--- server/upload.js
+++ server/upload.js
@@ -9,10 +9,11 @@ let uploads = [];
 
 // Cloudinary config from .env
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_URL.split('@')[1],
-  api_key: process.env.CLOUDINARY_URL.split('//')[1].split(':')[0],
-  api_secret: process.env.CLOUDINARY_URL.split(':')[2].split('@')[0],
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
 
 // Use memory storage (not disk)
 const storage = multer.memoryStorage();
